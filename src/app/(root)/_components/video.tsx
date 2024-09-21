f1381c49fc50dabdda01bc6c215a26de80208c2e
Mihir2423 <mihirraj444@gmail.com>
2024-09-21 23:11:25 +0530

    fix : video animation glitch

--- src/app/(root)/_components/video.tsx
+++ src/app/(root)/_components/video.tsx
@@ -5,45 +5,48 @@ import { useRef } from "react";
 
 export const Video = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const stickyRef = useRef<HTMLDivElement>(null);
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start end", "end start"],
+    offset: ["start start", "end start"],
   });
 
-  const clipPathProgress = useTransform(scrollYProgress, [0, 0.5], [10, 75]);
+  const clipPathProgress = useTransform(scrollYProgress, [0, 0.6], [20, 75]);
 
   return (
-    <div
-      ref={containerRef}
-      className="relative w-screen h-screen overflow-hidden"
-    >
-      <div className="relative z-auto inset-0 p-0 w-full h-full">
-        <motion.div
-          className="w-full h-full"
-          style={{
-            clipPath: useTransform(
-              clipPathProgress,
-              (value: number) => `circle(${value}% at 50% 50%)`
-            ),
-          }}
-        >
-          <video
-            className="absolute inset-0 w-full h-full object-cover"
-            src="/videos/about.mp4"
-            autoPlay
-            loop
-            muted
-          />
-        </motion.div>
-      </div>
+    <div ref={containerRef} className="relative h-[200vh]">
       <div
-        className="bottom-0 z-[10] absolute w-full h-[370px]"
-        style={{
-          maskImage: "linear-gradient(transparent, black 85%)",
-          backgroundColor: "rgb(12, 12, 12)",
-        }}
-      />
+        ref={stickyRef}
+        className="top-0 sticky w-screen h-screen overflow-hidden"
+      >
+        <div className="relative z-auto inset-0 p-0 w-full h-full">
+          <motion.div
+            className="w-full h-full"
+            style={{
+              clipPath: useTransform(
+                clipPathProgress,
+                (value: number) => `circle(${value}% at 50% 50%)`
+              ),
+            }}
+          >
+            <video
+              className="absolute inset-0 w-full h-full object-cover"
+              src="/videos/about.mp4"
+              autoPlay
+              loop
+              muted
+            />
+          </motion.div>
+        </div>
+        <div
+          className="bottom-0 z-[10] absolute w-full h-[370px]"
+          style={{
+            maskImage: "linear-gradient(transparent, black 85%)",
+            backgroundColor: "rgb(12, 12, 12)",
+          }}
+        />
+      </div>
     </div>
   );
 };
