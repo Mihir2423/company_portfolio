7f90d2321f590e3c4e0d0db861482735b11c33da
Mihir2423 <mihirraj444@gmail.com>
2024-09-22 13:11:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>
+      <body className={`${poppins.className} antialiased bg-black`}>
         <Toaster />
         <PreloaderWrapper>{children}</PreloaderWrapper>
       </body>
